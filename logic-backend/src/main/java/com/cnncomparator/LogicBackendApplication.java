df2924c6aeb0b4782b253e7636bbc7fd4299393a
Michal Andrzejczak <michandrzejczakofficial@gmail.com>
2026-06-30 19:26:36 +0200

    feat(logic-backend): initialize Spring Boot project structure

---
+++ logic-backend/src/main/java/com/cnncomparator/LogicBackendApplication.java
@@ -0,0 +1,12 @@
+package com.cnncomparator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogicBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogicBackendApplication.class, args);
+    }
+}
